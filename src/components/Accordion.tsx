fb8f61100dde7ae4073c6de91c2bfda6f712ffab
unchhohang <unchhohang@gmail.com>
2025-04-30 11:26:20 +0545

    Accordion fixed

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -2,8 +2,13 @@ type props = {
   question: string
 }
 
-export default function Accordion({question}: props) {
-  return(
-  <div>{question}</div>
+export default function Accordion({ question }: props) {
+  return (
+    <div className='flex justify-between bg-offWhite p-3 px-4  rounded-xl mb-2'>
+      <div>{question}</div>
+      <span className="text-2xl">+</span>
+    </div>
   );
 }
+
+
